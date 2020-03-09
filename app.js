e51f3412bb533238cc43e125200bcdb30f1756dc
MilenaGiachetti <giachettimilena@gmail.com>
2020-03-09 17:07:20 -0300

    Added order routes

--- app.js
+++ app.js
@@ -133,7 +133,6 @@ app.post('/users', (req,res) => {
                 is_admin   : 'FALSE'
             };
             let sql = `INSERT INTO users VALUES (:user_id, :username, :firstname, :lastname, :email, :adress, :phone, :password, :last_order, :is_admin)`;
-            //let sql = `INSERT INTO users VALUES (?, ?, ?, ?, ?, ?, ?, ?, 0, 'FALSE')`;
             sequelize.query( sql, {
                 replacements: user
             }).then(result => {
@@ -410,6 +409,239 @@ app.delete('/products/:id', authorizateUser, (req,res) => {
             res.json(`Eliminado con éxito producto con id: ${req.params.id}`);
         }
     })
+})
+
+/*---------------------------------------------ORDERS---------------------------------------------*/
+/*let sql = `SELECT username, firstname, lastname, email, adress, phone FROM users WHERE is_admin = 'FALSE'`;
+    sequelize.query( sql, {
+        type:sequelize.QueryTypes.SELECT
+    }).then(all_users => {
+        if (all_users.length === 0) {
+            res.status(404).send(`Error: no existe ningún usuarios`)
+        } else {
+            res.json(all_users);
+        }
+    })*/
+
+/*-----------------ADD AN ORDER-----------------*/
+app.post('/orders', authenticateUser, (req,res) => {
+    /*getting all products info to: 
+    -make the order description
+    -make the order total payment (I dont know if this is needed because the client should know before this info is sent to the back-end)
+    -make array of objects to send to the join db products_orders
+    */
+    let description = "";
+    let products_order = [];
+    let total_price = 0;
+    async function get(){
+        let product_quantity = Object.keys(req.body.products).length;
+        let product_number = 0;
+        for(let id in req.body.products){
+            let sql = `SELECT * FROM products WHERE product_id = ?`;
+            await sequelize.query( sql, {
+                replacements: [id], type:sequelize.QueryTypes.SELECT
+            }).then(result => {
+                product_number += 1;
+                description += req.body.products[id] + "x" + result[0].abbreviation + " ";
+                total_price += req.body.products[id]*(+result[0].price);
+                products_order.push({
+                    "product_id"      : id,
+                    "product_quantity": req.body.products[id],
+                    "user_id"         : req.user[0].user_id
+                })
+                /*Checking for the last iteration so the info can be sent to be inserted in the order and products_orders tables */
+                if(product_number === product_quantity){
+                    insert(products_order, description, total_price);
+                }
+            })
+        };
+    }
+    get();
+    function insert(products_order, description, total_price){
+        let order = {
+            description : description,
+            payment     : req.body.payment,
+            order_state : 'nuevo',
+            total_price : total_price,
+            user_id     : req.user[0].user_id
+        };
+        let sql = 'INSERT INTO orders SET description = :description, payment = :payment, order_state = :order_state, total_price= :total_price, user_id = :user_id';
+        sequelize.query( sql, {
+            replacements: order
+        }).then(result => {
+            let changed_user = {
+                user_id    : req.user[0].user_id,
+                username   : req.user[0].username,
+                firstname  : req.user[0].firstname,
+                lastname   : req.user[0].lastname,
+                email      : req.user[0].email,
+                adress     : req.user[0].adress,
+                phone      : req.user[0].phone,
+                password   : req.user[0].password,
+                last_order : result[0],
+                is_admin   : req.user[0].is_admin
+            };
+            let sql =  `UPDATE users SET user_id = :user_id, username = :username, firstname = :firstname, lastname = :lastname, email = :email, adress = :adress, phone = :phone, password = :password, last_order = :last_order, is_admin = :is_admin
+                        WHERE user_id = :user_id`;
+            sequelize.query( sql, {
+                replacements: changed_user
+            }).then((result) => {
+                console.log(result);
+            }).catch((err)=>{
+                console.log(err);
+            })
+            products_order.forEach(product => {
+                product["order_id"] = result[0];
+                let sql = `INSERT INTO products_orders SET order_id= :order_id, product_id = :product_id, product_quantity = :product_quantity, user_id = :user_id`;
+                sequelize.query( sql, {
+                    replacements: product
+                }).then((result)=>{
+                    console.log(result);
+                }).catch((err)=>{
+                    console.log(err);
+                })
+            })
+            res.status(200).send('Pedido creado');
+        }).catch((err)=>{
+            console.log(err);
+        })
+    }
+})
+
+/*example of info to send in the body:
+{
+    "payment": "efectivo",
+    "products": {
+    	"34": 3,
+        "2": 1
+    }
+}*/
+
+/*-----------------SEE ALL ORDERS-----------------*/
+app.get('/orders', authorizateUser, (req, res) => {
+    let sql = 'SELECT * FROM orders';
+    sequelize.query( sql, {
+        type:sequelize.QueryTypes.SELECT
+    }).then(all_orders => {
+        if (all_orders.length === 0) {
+            res.status(404).send(`Error: no hay ningún pedido en la base de datos`);
+        } else {
+            res.json(all_orders);
+        }
+    })
+})
+
+/*-----------------SEE A ORDER-----------------*/
+app.get('/orders/:id', authenticateUser, (req, res) => {
+    let sql =  `SELECT * FROM orders 
+                WHERE order_id = ?`;
+    sequelize.query( sql, {
+        replacements: [req.params.id], type:sequelize.QueryTypes.SELECT
+    }).then(result_order => {
+        if (result_order.length === 0) {
+            res.status(404).send("Pedido no existente");
+        } else {
+            if(req.user[0].user_id === result_order[0].user_id || req.user[0].is_admin === 'TRUE'){
+                let sql =  `SELECT * FROM products_orders 
+                WHERE order_id = ?`;
+                sequelize.query( sql, {
+                    replacements: [req.params.id], type:sequelize.QueryTypes.SELECT
+                }).then(result_order_products => {
+                    let allproducts = [];
+                    for(let i = 0; i < result_order_products.length; i++){
+                        let sql =  `SELECT * FROM products 
+                                    WHERE product_id = ?`;
+                        sequelize.query( sql, {
+                            replacements: [result_order_products[i].product_id], type:sequelize.QueryTypes.SELECT
+                        }).then(result_product => {
+                            allproducts.push({
+                                "quantity"     : result_order_products[i].product_quantity,
+                                "product_id"   : result_product[0].product_id,
+                                "product_name" : result_product[0].product_name,
+                                "abbreviation" : result_product[0].abbreviation,
+                                "link_img"     : result_product[0].link_img,
+                                "price"        : result_product[0].price
+                            });
+                            if (i === (result_order_products.length - 1)) {
+                                send_json(result_order, allproducts);
+                            }
+                        })
+                    }
+                })
+            } else {
+                res.status(403).send("Error: no se encuentra autorizado para ver esta información");
+            }
+        }
+    })
+    function send_json (result_order, allproducts){
+        result_order.productos = allproducts;
+        json = result_order.result_order;
+        console.log(result_order);
+        res.json({"pedido" : result_order, "productos" : allproducts});
+    }
+})
+
+/*-----------------UPDATE A ORDER-----------------*/
+app.put('/orders/:id', authorizateUser, (req,res) => {
+    let sql =  `SELECT * FROM orders 
+                WHERE order_id = ?`;
+    sequelize.query( sql, {
+        replacements: [req.params.id], type:sequelize.QueryTypes.SELECT
+    }).then(current_order => {
+        if (current_order.length === 0) {
+            res.status(404).send("Pedido no existente");
+        } else {
+            let changed_order = {
+                order_id    : req.params.id,
+                description : current_order[0].description,
+                payment     : current_order[0].payment,
+                order_state : req.body.order_state !== undefined ? req.body.order_state : current_order[0].order_state,
+                total_price : current_order[0].total_price,
+                user_id     : current_order[0].user_id
+            };
+            let sql =  `UPDATE orders 
+                        SET  description = :description, payment = :payment, order_state = :order_state, total_price= :total_price, user_id = :user_id
+                        WHERE order_id = :order_id`;
+            sequelize.query( sql, {
+                replacements: changed_order
+            }).then(order => {
+                console.log(order);
+                res.json((`Cambiado con éxito estado de pedido con id ${req.params.id} a '${changed_order.order_state}'`));
+            })
+        }
+    })
+});
+
+/*example of info to send in the body:
+{
+    "order_state" : "cancelado"
+}*/
+
+/*-----------------DELETE A ORDER-----------------*/
+app.delete('/orders/:id', authorizateUser, (req,res) => {
+    let sql =  `DELETE FROM orders
+                WHERE order_id = ?`;
+    sequelize.query( sql, {
+        replacements: [req.params.id]
+    }).then((order_result) => {
+        if (order_result[0].affectedRows === 0) {
+            res.status(404).send("Pedido no existente");
+        } else {
+            let sql =  `DELETE FROM products_orders
+                WHERE order_id = ?`;
+            sequelize.query( sql, {
+                replacements: [req.params.id]
+            }).then((product_result) => {
+                if (product_result[0].affectedRows === 0) {
+                    res.status(404).send("Pedido no existente");
+                } else {
+                    res.json(product_result);
+                    /*hace falta agregarle que chequee si el usuario al q corresponde lo tiene como ultima orden en cuyo caso borrarsela*/
+                }
+            })
+        }
+    })
+
 })
 
 /*---------------------------------------------USER LOG IN---------------------------------------------*/
