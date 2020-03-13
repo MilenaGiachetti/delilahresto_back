79ee8f6b8ab40d4756291eaf88990054ca05942c
MilenaGiachetti <giachettimilena@gmail.com>
2020-03-13 12:04:02 -0300

    Added hashing compare to login

--- app/controllers/users_controllers.js
+++ app/controllers/users_controllers.js
@@ -182,22 +182,26 @@ exports.deleteOne = (req, res) => {
 exports.login = (req,res) => {
     const jwtPass = reqs.jwtPass;
     let sql =  `SELECT * FROM users 
-                WHERE (username = :username OR email = :email) 
-                AND password = :password`;
+                WHERE (username = :username OR email = :email)`;
     sequelize.query( sql, {
-        replacements: {username : req.body.username , email: req.body.username,password : req.body.password}, type:sequelize.QueryTypes.SELECT
+        replacements: {username : req.body.username , email: req.body.username}, type:sequelize.QueryTypes.SELECT
     }).then(result => {
-        /*error management*/
-        if(result === undefined  || !(result.length > 0)){
-            res.status(401).send('Error: Datos incorrectos');
-        } else{
-            /*token created and sent when correct data is given*/
-            let user_id = result[0].user_id;
-            const token = reqs.jwt.sign({
-                user_id,
-            }, jwtPass);
-            res.json({token: token, user_id: user_id});
+        async function checkPass (){
+            const match = await reqs.bcrypt.compare(req.body.password, result[0].password);
+            console.log('1:' + match);
+            /*error management*/
+            if (match){
+                /*token created and sent when correct data is given*/
+                let user_id = result[0].user_id;
+                const token = reqs.jwt.sign({
+                    user_id,
+                }, jwtPass);
+                res.json({token: token, user_id: user_id});
+            } else {
+                res.status(401).send('Error: Datos incorrectos');
+            }
         }
+        checkPass();
     }).catch((err)=>{
         res.status(500).send( 'Error: ' + err );
     })
