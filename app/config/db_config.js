6296aa39ff314f17d32350b7600bae7f4df34560
MilenaGiachetti <giachettimilena@gmail.com>
2020-03-18 14:50:02 -0300

    Changed db name

--- app/config/db_config.js
+++ app/config/db_config.js
@@ -1,13 +1,9 @@
 /*---------------------------------------------CONNECTION TO DB---------------------------------------------*/
 const Sequelize   = require('sequelize');
-const sequelize   = new Sequelize('mysql://root:@localhost:3306/delilah_resto2', {
+const sequelize   = new Sequelize('mysql://root:@localhost:3306/delilah_resto', {
     dialectOptions: {
         multipleStatements: true
       }
 });
-/*let   db          = {};
-
-db.sequelize      = sequelize;
-db.Sequelize      = Sequelize;*/
 
 module.exports    = sequelize;
