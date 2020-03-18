9d3cbf92fc0cc2551867aba5dfe6e2e4f88d269a
MilenaGiachetti <giachettimilena@gmail.com>
2020-03-18 12:24:47 -0300

    improved error message

--- app/middlewares/middlewares.js
+++ app/middlewares/middlewares.js
@@ -9,7 +9,13 @@ const jwtPass = reqs.jwtPass;
 /*-----------------AUTHENTICATE A USER-----------------*/
 exports.authenticateUser = (req, res, next) => {
     if(req.headers.authorization === undefined){
-        res.status(401).send('Error: Error al validar usuario, ingrese a su cuenta para ver esta pagina');
+        res.status(401).json(
+            {"error": 
+                {"status": "401",
+                "message": "user validation error, log in to see this page"
+                }
+            }
+        )
     } else {
         const token = req.headers.authorization.split(' ')[1];
         const verifiedToken = reqs.jwt.verify(token, jwtPass);
@@ -19,14 +25,26 @@ exports.authenticateUser = (req, res, next) => {
             replacements: [verifiedToken.user_id], type:sequelize.QueryTypes.SELECT
         }).then(user => {
             if(user === undefined  || !(user.length > 0)){
-                res.status(401).send('Error: Error al validar usuario, ingrese a su cuenta para ver esta pagina');
+                res.status(401).json(
+                    {"error": 
+                        {"status": "401",
+                        "message": "user validation error, log in to see this page"
+                        }
+                    }
+                )
             } else {
                 /*user data is sent to routes that use this middleware in the request. Way of access: For example, access to user first name req.user[0].firstname !remember: info is sent as an array*/
                 req.user = user;
                 next();
             }
         }).catch((err)=>{
-            res.status(500).send( 'Error: ' + err );
+            res.status(500).json(
+                {"error": 
+                    {"status": "500",
+                    "message": "Internal Server Error: " + err
+                    }
+                }
+            )
         })
     }
 }
@@ -34,7 +52,13 @@ exports.authenticateUser = (req, res, next) => {
 exports.authorizateUser = (req, res, next) => {
     if(req.headers.authorization === undefined){
         /*403 or 401? Beacause if theres no header there hasnt been a log in*/
-        res.status(403).send('Error: No tiene acceso a esta pagina');
+        res.status(403).json(
+            {"error": 
+                {"status": "403",
+                "message": "user not authorized to see this information"
+                }
+            }
+        )
     } else {
         const token = req.headers.authorization.split(' ')[1];
         const verifiedToken = reqs.jwt.verify(token, jwtPass);
@@ -46,13 +70,25 @@ exports.authorizateUser = (req, res, next) => {
         }).then(user => {
             console.log(user);
             if(user === undefined  || !(user.length > 0)){
-                res.status(403).send('Error: No tiene acceso a esta pagina');
+                res.status(403).json(
+                    {"error": 
+                        {"status": "403",
+                        "message": "user not authorized to see this information"
+                        }
+                    }
+                )
             } else{
                 /*is given access to existent users that have ADMIN ROL*/
                 next();
             }
         }).catch((err)=>{
-            res.status(500).send( 'Error: ' + err );
+            res.status(500).json(
+                {"error": 
+                    {"status": "500",
+                    "message": "Internal Server Error: " + err
+                    }
+                }
+            )
         })
     }
 }
